32e881f19a436d6b0e5fb13c19d7a393c46167d4
platterss <78766794+platterss@users.noreply.github.com>
2024-01-12 23:57:49 -0800

    Added line breaks to main

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] array = getInput();
-        Matrix matrix = new Matrix(array);
+        Matrix matrix = new Matrix(getInput());
         System.out.println();
 
         System.out.println("Inputted matrix:");
